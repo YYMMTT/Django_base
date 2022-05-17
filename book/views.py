03ae0bee793995a4c348bbf4cecc10945331c3e3
yangmengting <yanjinmting@163.com>
2022-05-17 18:57:30 +0800

    定义视图函数

--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+视图函数就是Python函数
+
+视图函数参数必须是request，必须有返回值
+"""
+from django.http import HttpRequest
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse('ok')
